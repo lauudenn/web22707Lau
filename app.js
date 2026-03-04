c002382f51798568d5622c2f40f00c3d48c441a1
lauudenn <lauradeniss837@gmail.com>
2026-03-03 20:02:14 -0600

    tarea1

---
+++ app.js
@@ -0,0 +1,29 @@
+//Programa operaciones basicass
+
+//SUMA
+var numero1 = 2;
+var numero2 = 2;
+
+var suma = numero1+numero2;
+console.log("La suma de: "+numero1+"+"+numero2+"="+suma);
+
+//RESTA
+var num1 = 2;
+var num2 = 2;
+
+var resta = num1-num2;
+console.log("La resta de: "+num1+"-"+num2+"="+resta);
+
+//MULTIPLICACION
+var nu1 = 2;
+var nu2 = 2;
+
+var multiplicacion = nu1*nu2;
+console.log("La multiplicacion de: "+nu1+"x"+nu2+"="+multiplicacion);
+
+//DIVISION
+var n1 = 2;
+var n2 = 2;
+
+var division = n1/n2;
+console.log("La division de: "+num1+"/"+num2+"="+division);
