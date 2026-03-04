94d473a7261eba0e0d6ce4e2661b3601dc1f90ba
lauudenn <lauradeniss837@gmail.com>
2026-03-03 20:37:21 -0600

    tarea 2

---
+++ tarea2/app2.js
@@ -0,0 +1,24 @@
+// Operadores Logicos
+/*
+&&: Si los dos son v nos da un V
+||: con que unos sea v dara v
+!: negacion
+*/
+
+var compara = true && true;
+console.log(compara);
+
+var compara = true || true;
+console.log(compara);
+
+
+var compara = false || true;
+console.log(compara);
+
+//Programar el and y or 
+
+var and = 5>2 && 6 !== 8;
+console.log(and);
+
+var or = 9<10 || 5 < 2;
+console.log(or);
