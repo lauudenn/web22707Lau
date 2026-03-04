9d1227f4c110d910d0407829be20cc4ff2c6e2d0
lauudenn <lauradeniss837@gmail.com>
2026-03-03 20:48:53 -0600

    tarea 3

---
+++ tarea3/app3.js
@@ -0,0 +1,10 @@
+var numero1 = 38;
+var numero2 = 12;
+
+if(numero1>numero2){
+    console.log(numero1+" es el numero mayor y "+numero2+" es el menor");
+}else if(numero2>numero1){
+    console.log(numero2+" es el numero mayor y "+numero1+" es el menor");
+}else{
+    console.log("Son iguales")
+}
